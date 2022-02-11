1306b2f1e8f0b8a5dbf8a8391eab7304967c854e
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-02-11 17:14:59 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,24 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;   
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+
+    public double calcularSalario() {
+        if (tipo == 1) {
+            return salario;
+        }
+        else if (tipo == 2) {
+            return salario + comissao * salario;
+        }
+        else if (tipo == 3) {
+            return salario + bonus;
+        }
+        return 0;
+    }
 
     //getters/setters
     public String getNome() {
@@ -18,5 +36,5 @@ public class Empregado {
     public void setIdade(int idade) {
         this.idade = idade;
     }
-    
+
 }
