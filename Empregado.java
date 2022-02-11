96db1bc7ab9edaa5aee5f7a0ed3302033dbf74fa
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-02-11 17:49:04 -0300

    Metodos de acesso da variavel tipos adcionados. Tres empregados construidos

--- Empregado.java
+++ Empregado.java
@@ -37,4 +37,11 @@ public class Empregado {
         this.idade = idade;
     }
 
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
 }
